fa707e57b2e5e2a6b7443a3da6adba9a4c01f886
RenanaYehuda <66746638+RenanaYehuda@users.noreply.github.com>
2023-08-14 12:34:04 +0300

    Update contextProvider.js

--- src/contextProvider.js
+++ src/contextProvider.js
@@ -9,7 +9,7 @@ const ContextProvider = ({ children }) => {
   const [weather, setWeather] = useState({});
   const [lastSearch, setLastSearch] = useState([]);
   return (
-    <Context.Provider value={{ user, setUser, allCities, setAllCities, weather, setWeather,city, setCity }}>
+    <Context.Provider value={{ user, setUser, allCities, setAllCities, weather, setWeather,city, setCity, lastSearch, setLastSearch }}>
       {children}
     </Context.Provider>
   );
