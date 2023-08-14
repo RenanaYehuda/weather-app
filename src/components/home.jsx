faafa084a388137c8ceb1653d22a12b7d8b2c66d
Renana Yehuda <Renanay305@gmail.com>
2023-08-14 10:58:00 +0300

    update master

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,17 +7,16 @@ import {
   InputLabel,
   NativeSelect,
   Stack,
-  TextField,
 } from "@mui/material";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../contextProvider";
 import BigWeather from "./bigWeather";
 import LittleWeather from "./littleWeather";
-import { API_URL, doApiGet } from "../api/api";
 import { useForm } from "react-hook-form";
 import dataCountry from "../data/apiRequest.json";
-import { Padding } from "@mui/icons-material";
+import axios from "axios";
+const API_URL = "http://localhost:3001";
 
 const Home = () => {
   const days = [
@@ -27,23 +26,38 @@ const Home = () => {
     "בעוד 4 ימים",
     "בעוד 5 ימים",
   ];
-  const [isLoading, setIsLoading] = useState(false);
-  const { user, setAllCities, allCities, setWeather, weather } =
-    useContext(Context);
 
-  const [city, setCity] = useState("Jerusalem");
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    user,
+    city,
+    setCity,
+    setAllCities,
+    allCities,
+    weather,
+    setWeather,
+    lastSearch,
+    setLastSearch,
+  } = useContext(Context);
 
   const { handleSubmit } = useForm();
 
   const onSubForm = () => {
-    getCity(city);
+    console.log(city);
+    createLastSearch();
+    getCity();
   };
 
-  const getCity = async (city) => {
+  const getCity = async () => {
     console.log(city);
     let url = API_URL + `/cities/${city}`;
     try {
-      let resp = await doApiGet(url, user);
+      let resp = await axios.get(url, {
+        headers: {
+          user_name: user ? user.User_Name : null,
+          user_mispar_ishi: user ? user.Mispar_Ishi : null,
+        },
+      });
       if (resp) {
         console.log(resp.data);
         getWeather(resp.data);
@@ -68,20 +82,50 @@ const Home = () => {
             city.lat == lat_lon.latitude.toFixed(4) &&
             city.lon == lat_lon.longitude.toFixed(4)
         );
-        console.log(city);
-        setWeather(city);
+        if (city) {
+          setWeather(city);
+          console.log("haveeeeee");
+          console.log(weather);
+        } else {
+          setWeather("");
+          console.log("nooooooooo");
+          console.log(weather);
+        }
       }
     } catch (err) {
+      setWeather("");
       console.log(err.message);
       alert("User or password worng, or service down");
     }
+  };
+
+  const createLastSearch = () => {
+    let searchs = lastSearch;
+    let detailesCity = allCities.filter((item) => item.city === city);
+    if (detailesCity) {
+      detailesCity = detailesCity[0];
+      if (searchs.length < 5) {
+        console.log(detailesCity);
+        searchs.unshift(detailesCity);
+      } else {
+        searchs.pop();
+        searchs.unshift(detailesCity);
+      }
+      console.log(searchs);
+      setLastSearch(searchs);
+    } else console.log("City not exsist");
   };
 
   useEffect(() => {
     const getCities = async () => {
       let url = API_URL + "/getAllCities";
       try {
-        let resp = await doApiGet(url, user);
+        let resp = await axios.get(url, {
+          headers: {
+            user_name: user ? user.User_Name : null,
+            user_mispar_ishi: user ? user.Mispar_Ishi : null,
+          },
+        });
         if (resp.status == 200) {
           console.log(resp.data);
           setAllCities(resp.data);
@@ -97,7 +141,7 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    getCity(city);
+    getCity();
   }, []);
 
   return (
@@ -127,32 +171,43 @@ const Home = () => {
               >
                 :Choose country
               </InputLabel>
-              <NativeSelect
-                onChange={(e) => setCity(e.target.value)}
-                defaultValue={city}
-                inputProps={{
-                  name: "city",
-                  id: "uncontrolled-native",
-                }}
-                sx={{direction: "ltr"}}
-              >
-                {allCities.map((item, i) => (
-                  <option key={i} value={item.city}>
-                    {item.city}
-                  </option>
-                ))}
-              </NativeSelect>
-              <IconButton aria-label="search" size="large" type="submit">
-                <SearchRoundedIcon />
-              </IconButton>
+              <Box>
+                <IconButton aria-label="search" size="large" type="submit">
+                  <SearchRoundedIcon />
+                </IconButton>
+                <NativeSelect
+                  onChange={(e) => setCity(e.target.value)}
+                  defaultValue={city}
+                  inputProps={{
+                    name: "city",
+                    id: "uncontrolled-native",
+                  }}
+                  sx={{ direction: "ltr", width: 700, marginTop: 4 }}
+                >
+                  {allCities.map((item, i) => (
+                    <option key={i} value={item.city}>
+                      {item.city}
+                    </option>
+                  ))}
+                </NativeSelect>
+              </Box>
             </FormControl>
           </Box>
-          <BigWeather city={city} />
-          <Stack direction={"row"} sx={{position: 'absolute', bottom: '-20%'}}>
-            {days.map((item, i) => (
-              <LittleWeather k={i} day={item} />
-            ))}
-          </Stack>
+          {weather ? (
+            <>
+              <BigWeather />
+              <Stack
+                direction={"row"}
+                sx={{ position: "absolute", bottom: "-20%" }}
+              >
+                {days.map((item, i) => (
+                  <LittleWeather key={i} k={i} day={item} />
+                ))}
+              </Stack>
+            </>
+          ) : (
+            <h3> Weather not exsist</h3>
+          )}
         </Box>
       ) : (
         <CircularProgress
